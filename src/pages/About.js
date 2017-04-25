61ec3fc2d97aa64c6daf90e72ea5223cad2d701d
monkeyone1 <a793472281@qq.com>
2017-04-25 23:56:27 +0800

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,40 +1,75 @@
 import React, {Component} from 'react';
 import Paper from 'material-ui/Paper';
 import me from '../image/me.jpg';
+import marked from 'marked';
+import echarts from 'echarts';
 
 class About extends Component {
+
+ componentDidMount(){
+    var myChart = echarts.init(document.getElementById('main'));
+    myChart.setOption({
+      title: {
+        text: '我的技能展示',
+        textStyle:{
+          color: 'teal'
+        },
+        left:'center'
+      },
+      tooltip: {},
+      xAxis: {
+          data: ['html','css','js','react','node','mongodb']
+      },
+      yAxis: {},
+      series: [{
+          name: '熟练度',
+          type: 'bar',
+          data: [90, 95, 85, 80, 70, 60]
+      }]
+    });
+  }
   render() {
     return (
-      <div style={{
+      <div
+        style={{
         width: '100%',
-        display:'flex',
-        flexDirection:'column',
-        
-        
-        
+        display: 'flex',
+        flexDirection: 'column'
       }}>
-        <Paper zDepth={1} style={{
-          display:'flex',
-          margin:20
-          
+        <Paper
+          zDepth={1}
+          style={{
+          display: 'flex',
+          margin: '20px'
         }}>
           <Paper
             style={{
             height: 200,
             width: 200,
             margin: 20,
-            overflow:'hidden'
+            overflow: 'hidden'
           }}
             zDepth={1}
-            circle={true}
-            
-            >
+            circle={true}>
 
-            <img style={{
-              width:200
-            }}src={me} alt=""/>
-            </Paper>
+            <img src={me} style={{
+              width: 200
+            }} alt=""/>
+          </Paper>
+           <div style={{ margin:'40px'}}>
+             <h1>张振东</h1>
+             <p>一个幽默的前端程序员</p>
+
+
+           </div>
+
         </Paper>
+     
+
+      <div id='main' style={{width:'100%',height:'600px',maxWidth:'600px',margin:'0 auto'}}></div>
+     
+      
+        
       </div>
     );
   }
